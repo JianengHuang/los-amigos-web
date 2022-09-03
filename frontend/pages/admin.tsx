0cf98e131dd1ebd2c638e350d7bed1968be457f8
Jianeng Huang <jianengh@gmail.com>
2022-09-03 11:25:05 +0200

    edited navbar

--- frontend/pages/admin.tsx
+++ frontend/pages/admin.tsx
@@ -1,140 +1,23 @@
-import { useState } from 'react';
-
-import { Button } from '@chakra-ui/button';
-
-import {
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  Heading,
-  VStack,
-} from '@chakra-ui/react';
-
-import { Formik } from 'formik';
-
-import * as Yup from 'yup';
-
-import TextField from '../components/FormField/TextField';
-
-import axios, { AxiosError, AxiosResponse } from 'axios';
-
-import { AlertStatus } from '@chakra-ui/react';
-
-import { emailRegEx, passwordRegEx } from '../utils/regex';
+import { Heading, VStack, Button } from '@chakra-ui/react';
+import { useDisclosure } from '@chakra-ui/react';
+import { useState, useRef } from 'react';
 
 const Admin = () => {
-  const [loading, setLoading] = useState(false);
-  const [status, setStatus] = useState<AlertStatus>('info');
-  const [message, setMessage] = useState('');
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [scrollBehavior, setScrollBehavior] = useState('inside');
+  const btnRef = useRef(null);
 
   return (
-    <Formik
-      initialValues={{
-        name: '',
-        ingredients: [''],
-        price: 0,
-        image: '',
-        category: '',
-      }}
-      validationSchema={Yup.object({
-        name: Yup.string().required('Nombre es necesario'),
-        ingredients: Yup.array().of(
-          Yup.string().required('Ingrediente es necesario')
-        ),
-        price: Yup.number().required('Precio es necesario'),
-        image: Yup.string().required('Imagen es necesario'),
-        category: Yup.string().required('Categoria es necesario'),
-      })}
-      onSubmit={(values: any, actions: any) => {
-        setLoading(true);
-        axios
-          .post('http://localhost:4000/dish/', values, {
-            withCredentials: true,
-          })
-          .then((res: AxiosResponse) => {
-            console.log(res);
-            if (res.status === 200) {
-              setStatus('success');
-              setMessage('Plato creado correctamente');
-            } else {
-              setStatus('error');
-              setMessage(res.data);
-            }
-            setLoading(false);
-          })
-          .catch((err: AxiosError) => {
-            setStatus('error');
-            console.log('error', err);
-            setMessage(err.message);
-            setLoading(false);
-          });
-        actions.resetForm();
-      }}
-    >
-      {(formik) => (
-        <VStack
-          as='form'
-          mx='auto'
-          w={{ base: '90%', md: 500 }}
-          h='100vh'
-          justifyContent='center'
-          //@ts-ignore
-          onSubmit={formik.handleSubmit}
-        >
-          <Heading>Añadir Plato</Heading>
-          {status !== 'info' ? (
-            <Alert status={status}>
-              <AlertIcon />
-              <AlertTitle>{message}</AlertTitle>
-            </Alert>
-          ) : null}
-
-          <TextField
-            name='name'
-            label='Nombre'
-            placeholder='Escribe el nombre del plato...'
-          ></TextField>
-
-          <TextField
-            name='ingredients'
-            label='Ingredientes'
-            type='array'
-            placeholder='Escribe los ingredientes...'
-          ></TextField>
-
-          <TextField
-            name='price'
-            label='Precio'
-            type='number'
-            placeholder='Escribe el precio...'
-          ></TextField>
-
-          <TextField
-            name='image'
-            label='Imagen'
-            type='string'
-            placeholder='Escribe el link de la photo...'
-          ></TextField>
-
-          <TextField
-            name='category'
-            label='Categoria'
-            type='string'
-            placeholder='Escribe la categoria...'
-          ></TextField>
-
-          <Button
-            isLoading={loading ? true : false}
-            type='submit'
-            variant='outline'
-            colorScheme='teal'
-          >
-            Crear Cuenta
-          </Button>
-          <img src='https://i.imgur.com/8nLFCVP.png' alt='pepe' />
-        </VStack>
-      )}
-    </Formik>
+    <>
+      <Heading as='h1' size='xl' textAlign='center' my={4}>
+        Admin
+      </Heading>
+      <VStack spacing={4} w='100%' maxW='500px' mx='auto'>
+        <Button mt={3} ref={btnRef} onClick={onOpen} color={'teal.100'} >
+          Add Recipe
+        </Button>
+      </VStack>
+    </>
   );
 };
 
