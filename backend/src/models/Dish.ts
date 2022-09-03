f74a8ecb28324a23a02344e06651931f69b44017
Jianeng Huang <jianengh@gmail.com>
2022-09-03 08:51:58 +0200

    added id to schema

--- backend/src/models/Dish.ts
+++ backend/src/models/Dish.ts
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 
 const dishSchema = new mongoose.Schema({
+  id: {
+    type: number,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
