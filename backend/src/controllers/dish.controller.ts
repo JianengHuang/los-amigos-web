6c6dc9837e376895f0f97721224fede598c25692
Jianeng Huang <jianengh@gmail.com>
2022-09-03 08:51:58 +0200

    added id to schema

--- backend/src/controllers/dish.controller.ts
+++ backend/src/controllers/dish.controller.ts
@@ -3,36 +3,25 @@ import { DishInterface } from '../interfaces/DishInterface';
 import Dish from '../models/Dish';
 
 export const createDish = async (req: Request, res: Response) => {
-  const { name, ingredients, price, image, category } = req?.body;
+  const { id, name, ingredients, price, image, category } = req?.body;
   if (
+    !id ||
     !name ||
     !ingredients ||
     !price ||
     !image ||
     !category ||
+    typeof id !== 'number' ||
     typeof name !== 'string' ||
-    typeof ingredients !== 'object' ||
+    ingredients.length === 0 ||
     typeof price !== 'number' ||
     typeof image !== 'string' ||
     typeof category !== 'string'
   ) {
-    console.log(
-      req.body,
-      !name,
-      !ingredients,
-      !price,
-      !image,
-      !category,
-      typeof name !== 'string',
-      typeof ingredients !== 'object',
-      typeof price !== 'number',
-      typeof image !== 'string',
-      typeof category !== 'string'
-    );
     res.status(400).json({ error: 'Incorrect Values' });
     return;
   }
-  Dish.findOne({ name }, async (err: Error, doc: DishInterface) => {
+  Dish.findOne({ id }, async (err: Error, doc: DishInterface) => {
     if (err) throw err;
     if (doc) {
       res.status(400).json({ error: 'Dish already exists' });
@@ -41,6 +30,7 @@ export const createDish = async (req: Request, res: Response) => {
     if (!doc) {
       try {
         const newDish = new Dish({
+          id,
           name,
           ingredients,
           price,
@@ -49,8 +39,8 @@ export const createDish = async (req: Request, res: Response) => {
         });
         await newDish.save();
         res.status(200).json({ message: 'Dish created' });
-      } catch (e: any) {
-        res.status(500).json({ error: e.message });
+      } catch (err: any) {
+        res.status(500).json({ error: err.message });
       }
     }
   });
