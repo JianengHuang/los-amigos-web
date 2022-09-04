0cccfaea49b31d04f3d06ed3d53bcdf930bdc644
Jianeng Huang <jianengh@gmail.com>
2022-09-04 11:31:14 +0200

    Added modal to add dishes

---
+++ frontend/pages/admin/formData.ts
@@ -0,0 +1,34 @@
+const formData = [
+  {
+    name: 'id',
+    label: 'ID',
+    type: 'number',
+    placeholder: '26',
+  },
+  {
+    name: 'name',
+    label: 'Nombre',
+    type: 'text',
+    placeholder: 'Arroz tres delicias',
+  },
+  {
+    name: 'ingredients',
+    label: 'Ingredientes',
+    type: 'array',
+    placeholder: 'Arroz, zanahoria, huevo',
+  },
+  {
+    name: 'price',
+    label: 'Precio',
+    type: 'number',
+    placeholder: '5.50',
+  },
+  {
+    name: 'category',
+    label: 'Categoria',
+    type: 'string',
+    placeholder: 'Arroces',
+  },
+];
+
+export default formData;
