89d9b3b457ce7b1d938a2d5796aae206825d59b1
Jianeng Huang <jianengh@gmail.com>
2023-02-25 10:19:32 +0100

    edited ecosystem

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       host: '13.36.39.37',
       ref: 'origin/next-13',
       repo: 'git@github.com:JianengHuang/los-amigos-web.git',
-      path: '/home/ubuntu',
+      path: '/home/ubuntu/los-amigos-web/frontend',
       'pre-deploy-local': '',
       'post-deploy':
         'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
