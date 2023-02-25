094c5ab86d9b6b81144ee00701a4ab20e80914cc
Jianeng Huang <jianengh@gmail.com>
2023-02-25 09:56:52 +0100

    add ecosystem

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      script: 'npm start',
+      watch: '.',
+    },
+    {
+      script: './service-worker/',
+      watch: ['./service-worker'],
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: 'C:\\Users\\USERNAME\\.ssh\\AWS\\AWS_KEY.pem',
+      user: 'ubuntu',
+      host: '13.36.39.37',
+      ref: 'origin/next-13',
+      repo: 'git@github.com:JianengHuang/los-amigos-web.git',
+      path: '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes',
+    },
+  },
+};
