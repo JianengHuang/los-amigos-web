4159a4b4181142a46b48ab1196fabc34f4663e0c
Jianeng Huang <jianengh@gmail.com>
2023-08-11 15:54:57 +0200

    add request method + path to logger

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 const logger = async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.method, req.path)
   console.log(req.body);
   next();
 };
