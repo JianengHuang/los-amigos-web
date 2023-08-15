bc6617663ceb2951bf56c206e8a330d8aef490b3
Jianeng Huang <jianengh@gmail.com>
2023-08-15 20:58:55 +0200

    add category type + return type in fetch functions

--- apps/client/utils/getCategories.ts
+++ apps/client/utils/getCategories.ts
@@ -1,4 +1,6 @@
-export default async function getCategories() {
+import { Category } from 'types/Category';
+
+export default async function getCategories(): Promise<Category[]> {
 	const response = await fetch(
 		`http://${process.env.API_HOST}:${process.env.API_PORT}/category`,
 		{
--- apps/client/utils/getDishes.ts
+++ apps/client/utils/getDishes.ts
@@ -1,4 +1,6 @@
-export default async function getDishes() {
+import { Dish } from 'types/Dish';
+
+export default async function getDishes(): Promise<Dish[]> {
 	const response = await fetch(
 		`http://${process.env.API_HOST}:${process.env.API_PORT}/dish`,
 		{
