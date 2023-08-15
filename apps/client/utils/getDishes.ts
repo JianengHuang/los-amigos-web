97c01b30f01717cc3581e8732ec6b9dfdd28f9a1
JianengHuang <jianengh@gmail.com>
2023-08-15 15:53:55 +0200

    add tag to fetch api

--- apps/client/utils/getDishes.ts
+++ apps/client/utils/getDishes.ts
@@ -2,7 +2,9 @@ export default async function getDishes() {
 	const response = await fetch(
 		`http://${process.env.API_HOST}:${process.env.API_PORT}/dish`,
 		{
-			cache: 'no-cache',
+			next: {
+				tags: ['dish'],
+			},
 		},
 	);
 	const dishes = await response.json();
