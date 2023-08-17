3b6319ed25851a7073fd22a04b415aa3115a0b91
Jianeng Huang <jianengh@gmail.com>
2023-08-17 15:00:01 +0200

    add types to useOutsideClick

--- apps/client/hooks/useOutsideClick.tsx
+++ apps/client/hooks/useOutsideClick.tsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'react';
+import { RefObject, useEffect } from 'react';
 
-export default function useOutsideClick(ref, callback) {
-	const handleClick = (e) => {
-		if (ref.current && !ref.current.contains(e.target)) {
+export default function useOutsideClick(
+	ref: RefObject<HTMLDivElement>,
+	callback: () => void,
+) {
+	const handleClick = ({ target }: MouseEvent): void => {
+		if (ref.current && !ref.current.contains(target as Node)) {
 			callback();
 		}
 	};
