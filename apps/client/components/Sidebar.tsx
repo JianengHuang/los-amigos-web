07dea509dae4d53bf9c483e1a5267b98784f478c
Jianeng Huang <jianengh@gmail.com>
2023-08-18 15:21:26 +0200

    add SidebarElement component

--- apps/client/components/Sidebar.tsx
+++ apps/client/components/Sidebar.tsx
@@ -3,6 +3,8 @@
 import useOutsideClick from 'hooks/useOutsideClick';
 import { Dispatch, SetStateAction, useRef } from 'react';
 import Image from 'next/image';
+import SidebarElement from './SidebarElement';
+import { usePathname } from 'next/navigation';
 
 type PropsType = {
 	sidebarIsOpen: boolean;
@@ -11,19 +13,20 @@ type PropsType = {
 
 const Sidebar = ({ sidebarIsOpen, setSidebarIsOpen }: PropsType) => {
 	const boxRef = useRef(null);
+	const pathname = usePathname();
 	useOutsideClick(boxRef, sidebarIsOpen, () => setSidebarIsOpen(false));
 	return (
 		<div
 			ref={boxRef}
 			className={`${
 				sidebarIsOpen ? 'translate-x-0' : '-translate-x-full'
-			} fixed left-0 top-0 z-50 h-screen w-64 rounded-r-3xl bg-yellow-200 shadow-2xl duration-300 ease-in-out`}
+			} fixed left-0 top-0 z-50 flex h-screen w-64 flex-col rounded-r-3xl bg-yellow-200 shadow-2xl duration-300 ease-in-out`}
 		>
 			<div
 				className={`${
 					sidebarIsOpen ? 'translate-x-0' : '-translate-x-full'
-				} m-3 flex w-[104px] cursor-pointer  flex-row items-center justify-center 
-				gap-2 rounded-md bg-red-200 pl-1 hover:bg-red-400 md:m-6 md:w-[125px]`}
+				} fixed m-3 flex max-h-9 w-[104px]  cursor-pointer flex-row items-center 
+				justify-center gap-2 rounded-md bg-red-200 pl-1 hover:bg-red-400 md:m-6 md:w-[125px]`}
 				onClick={() => setSidebarIsOpen(false)}
 			>
 				<Image
@@ -34,6 +37,10 @@ const Sidebar = ({ sidebarIsOpen, setSidebarIsOpen }: PropsType) => {
 					className="md:h-[30px] md:w-[30px]"
 				/>
 				<p className="text-2xl text-red-900 md:text-3xl">Cerrar</p>
+			</div>
+			<div className="relative top-24 flex flex-col gap-3">
+				<SidebarElement name="Carta" href="/" pathname={pathname} />
+				<SidebarElement name="Admin" href="/admin" pathname={pathname} />
 			</div>
 		</div>
 	);
