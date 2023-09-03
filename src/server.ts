637f5b20f281bb9ca28929866bd1c8f3fc047277
Jianeng Huang <jianengh@gmail.com>
2023-09-03 16:12:13 +0200

    add cors header

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,12 @@ import categoryRoutes from "./routes/category.routes";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  next();
+});
+
 app.use(express.json());
 
 app.use("/dish", dishRoutes);
