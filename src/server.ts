ed038194f7ac71817743a1ce8df6a8e70866d2c0
Jianeng Huang <jianengh@gmail.com>
2023-09-05 15:49:53 +0200

    add allow headers cors

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,8 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   next();
 });
 
